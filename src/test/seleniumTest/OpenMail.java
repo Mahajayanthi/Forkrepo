68515c8a96dc6608dd9a66c70d4aad05efcc1df1
Jujupretha Baskaran <jujuprethab@stg.com>
2022-01-24 13:08:37 +0530

    juju

---
+++ src/test/seleniumTest/OpenMail.java
@@ -0,0 +1,8 @@
+public class OpenMail {
+    public static void main(String selvi[]){
+
+        System.out.println("juju");
+    }
+}
+
+
---
+++ src/test/seleniumTest/TestDemoScreen.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestDemoScreen {
+
+    static WebDriver driver;
+
+    public static void main(String args[]) throws InterruptedException, IOException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(1000);
+        driver.manage().window().maximize();
+        driver.navigate().to("https://webmail.stgit.com/");
+        JavascriptExecutor js = (JavascriptExecutor)  driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        driver.findElement(By.linkText("proceed to webmail.stgit.com (unsafe)")).click();
+        driver.findElement(By.cssSelector("input.signInInputText")).sendKeys("stg\\jujuprethab");
+        driver.findElement(By.cssSelector("input[type*=\'assw\']")).sendKeys("Pretha@123");
+        Thread.sleep(100);
+        driver.findElement(By.className("siginTxt")).click();
+        Thread.sleep(1000);
+        driver.get("https://webmail.stgit.com/");
+        takeScreenShot(("Logic_page"));
+
+}
+    public static void takeScreenShot(String fileName) throws IOException {
+        File file = ((TakesScreenshot)driver).getScreenshotAs((OutputType.FILE));
+        FileUtils.copyFile(file, new File("C:\\Users\\jujuprethab\\eclipse-workspace\\Selenium_Test\\Screenshot\\"
+                +fileName+"juju.png"));
+    }
+}
+
+
